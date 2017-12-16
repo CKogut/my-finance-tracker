ccbb6f78a497597fbff3fc303be0510a91b8b4b7
Corinne <corinnekogut@gmail.com>
2017-12-16 17:56:23 +0000

    Initial commit of app

--- config/application.rb
+++ config/application.rb
@@ -2,13 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'twitter'
- 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "2kXkXyDjdc5fuIPxdEPKsBR1o"
-  config.consumer_secret     = "7QUS6b0Fk9F4T6ZnGplteuiLizT9rQBnaAQ6elsStfGM7gaFN5"
-  config.access_token        = "930457303291236353-zV30R3mfEIlEQuFG92A7kavce6qJ8Ir"
-  config.access_token_secret = "CRI5Gt4ORQrRFAZwDEj0wqIQpWKm6nqm560Knny9wcjHv"
-end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -30,5 +23,9 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    
+    
+    
   end
 end
